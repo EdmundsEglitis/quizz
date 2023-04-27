43f8090895003d3ce372ce590267708e255ebecd
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-04-27 11:38:05 +0300

    dark/light

---
+++ index.js
@@ -0,0 +1,16 @@
+const toggleTheme = document.getElementById("toggle-theme");
+const html = document.getElementById("html");
+
+toggleTheme.addEventListener("change", toggleThemeHandler)
+
+function toggleThemeHandler(){
+    console.log (toggleTheme.checked);
+
+
+
+if(toggleTheme.checked){
+    html.dataset.theme = "dark";
+}
+else{
+    html.dataset.theme = "light";
+}}
